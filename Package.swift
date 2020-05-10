f5a340e7cfa67a6ec314343dc8ecd9e40890d56b
Jann Schafranek <me@jannthomas.com>
2020-05-10 16:54:59 +0200

    Update Package

--- Package.swift
+++ Package.swift
@@ -1,8 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
-    name: "CSSH",
-    pkgConfig: "libssh2"
+    name: "CLibpq",
+    products: [
+        .library(name: "libssh2", targets: ["libssh2"])
+    ],
+    targets: [
+        .systemLibrary(name: "libssh2", pkgConfig: "libssh2")
+    ]
 )
